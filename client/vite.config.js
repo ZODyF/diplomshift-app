58c52c8270dc648c3f03bca910e819031a2b41f9
ZODy <145047630+ZODyF@users.noreply.github.com>
2026-06-23 01:53:14 +0300

    Add files via upload

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/diplom.github.io/',
+  base: '/',
   plugins: [react()],
 })
